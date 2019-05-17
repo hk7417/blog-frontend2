883cb8018a620a5e1c0f73f89b69365db56f43ea
hk7417 <hk7417@ajou.ac.kr>
2019-05-17 23:52:09 +0900

    success Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { ListPage, PostPage, EditorPage, NotFoundPage } from './pages';
+import { ListPage, PostPage, EditorPage, NotFoundPage } from 'pages';
 //import  ListPage  from 'pages/ListPage';
 //import  PostPage  from 'pages/PostPage';
 //import  Editor  from './Editor';
 //import  NotFoundPage  from 'pages/NotFoundPage';
-
+//components에 page를 만든 이유는 여기서 모듈을 불러올 때 기본 주소가 src가 아니라 src/components가 되어서.
+//어떻게 src로 바꿔야 하는지 모르겠음.
 const App = () => {
   return (
     <div>
